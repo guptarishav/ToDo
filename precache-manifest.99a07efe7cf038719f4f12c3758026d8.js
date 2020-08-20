9a4d49e9e75c56e9fec37ea154607d72cc712e65
Rishav <kumar.164@iitj.ac.in>
2020-08-20 23:36:20 +0530

    Updates

---
+++ precache-manifest.99a07efe7cf038719f4f12c3758026d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831c3cdcbc9addefc4fa58e2f41b5626",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "5c2291e82688d210afc0",
+    "url": "/ToDo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf54d416b0b48a1ecdd4",
+    "url": "/ToDo/static/css/main.485e02f3.chunk.css"
+  },
+  {
+    "revision": "5c2291e82688d210afc0",
+    "url": "/ToDo/static/js/2.5a50411f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.5a50411f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf54d416b0b48a1ecdd4",
+    "url": "/ToDo/static/js/main.0b4a3569.chunk.js"
+  },
+  {
+    "revision": "9b386be1435831774989",
+    "url": "/ToDo/static/js/runtime-main.1a6fee32.js"
+  }
+]);
